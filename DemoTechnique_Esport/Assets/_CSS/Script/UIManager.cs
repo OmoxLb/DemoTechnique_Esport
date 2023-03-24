80677aab10b917fe5962bd190a5b0011cc1ceef3
OmoxLb <Lecluse.theo@gmail.com>
2023-03-24 21:20:03 +0100

    Adding to list
    Creation de data et de liste, j'ai réussi à ajouter une liste avec des donne contenu dans un scriptable object,
    Juste à revoir le style des object pour qu'il fit bien dans la liste

--- DemoTechnique_Esport/Assets/_CSS/Script/UIManager.cs
+++ DemoTechnique_Esport/Assets/_CSS/Script/UIManager.cs
@@ -1,5 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEditor;
+using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 using UnityEngine.UIElements;
 
@@ -25,6 +28,14 @@ public class UIManager : MonoBehaviour
     [Tooltip("Scriptable object contenant les datas des players")]
     private PlayerList _playerList;
 
+    //Ui element contains list of player
+    private ListView _viewPlayerList;
+    //Button for add or remove player
+    private Button _addButton;
+    private Button _removeButton;
+    //The InputName for the name of the player
+    private TextField _inputName;
+
 
     #endregion
 
@@ -44,6 +55,11 @@ public class UIManager : MonoBehaviour
         Initialize();
     }
 
+    private void Update()
+    {
+        
+    }
+
     #endregion
 
     #region Customs Methods
@@ -53,6 +69,106 @@ public class UIManager : MonoBehaviour
     /// </summary>
     private void Initialize()
     {
+        SetInputName();
+        SetButtonList();
+        SetListView();
+
+    }
+
+    /// <summary>
+    /// Initialze the button for add or remove the list
+    /// </summary>
+    private void SetButtonList()
+    {
+        _addButton = _rootEsportUI.Q<Button>("AddList");
+
+        _addButton.clickable.clicked += AddValueToList;
+
+        if(string.IsNullOrWhiteSpace(_inputName.value))
+        {
+            _addButton.SetEnabled(false);
+        }
+    }
+
+
+    /// <summary>
+    /// Add a value to the list and reset the name selection
+    /// </summary>
+    private void AddValueToList()
+    {
+        if (_inputName == null) return;
+
+        _inputName.value = "";
+
+
+    }
+
+    /// <summary>
+    /// Set the input name and the event link
+    /// </summary>
+    private void SetInputName()
+    {
+        _inputName = _rootEsportUI.Q<TextField>("InputNom");
+
+        //Disable add button if name input is empty
+        _inputName.RegisterValueChangedCallback(evt =>
+        {
+            if (string.IsNullOrWhiteSpace(evt.newValue))
+                _addButton.SetEnabled(false);
+            else _addButton.SetEnabled(true);
+        });
+    }
+
+
+    /// <summary>
+    /// Initialize the list view
+    /// </summary>
+    private void SetListView()
+    {
+        //PART LISTE : 
+        //Initialize Ui Element :
+        _viewPlayerList = _rootEsportUI.Q<ListView>("PlayerListView");
+        _viewPlayerList.fixedItemHeight = _playerList.heightMaxList;
+
+
+        //Appear a scroll bar to the list view
+        _viewPlayerList.horizontalScrollingEnabled = true;
+
+        _viewPlayerList.makeItem = CreateItemList;
+        _viewPlayerList.bindItem = BindListViewItem;
+        _viewPlayerList.itemsSource = _playerList.players;
+        _viewPlayerList.Rebuild();
+    }
+
+
+    /// <summary>
+    /// Create the visual element for the players
+    /// </summary>
+    /// <param name="player"></param>
+    /// <returns></returns>
+    private VisualElement CreateItemList()
+    {
+        VisualElement visualElement = _playerUI.CloneTree();
+
+
+        Debug.Log(visualElement);
+
+
+        return visualElement;
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="listItem"></param>
+    /// <param name="index"></param>
+    private void BindListViewItem(VisualElement listItem, int index)
+    {
+        Label textID = listItem.Q<Label>("ID");
+        Label textName = listItem.Q<Label>("Name");
+
+        textID.text = _playerList.players[index].id.ToString();
+        textName.text = _playerList.players[index].name.ToString();
 
     }
 
