a9507595f4d2c67f505949a32d602bbbfa730332
OmoxLb <Lecluse.theo@gmail.com>
2023-03-25 18:43:00 +0100

    List View add item
    List view add item fonctionelle
    On peut déplacer les items dans la liste
    La liste se Re Order correctement, les id change correctement
    Polish : UI Selection en cours

---
+++ DemoTechnique_Esport/Assets/_CSS/Script/IComparerListByIndex.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class IComparerListByIndex : IComparer<Player>
+{
+    /// <summary>
+    /// Compare les index et les remet dans l'ordre
+    /// </summary>
+    /// <param name="visual1"></param>
+    /// <param name="visual2"></param>
+    /// <returns></returns>
+    public int Compare(Player visual1, Player visual2)
+    {
+        if (visual1.id > visual2.id)
+        {
+            return 1;
+        }
+        else return -1;
+    }
+}
--- DemoTechnique_Esport/Assets/_CSS/Script/PlayerList.cs
+++ DemoTechnique_Esport/Assets/_CSS/Script/PlayerList.cs
@@ -16,6 +16,11 @@ public class Player
     public string name;
     public int id;
 
+    public Player (string name, int id)
+    {
+        this.name = name;
+        this.id = id;
+    }
     
 }
 
